712bf80cb10a694b71fc0768e3c8c5b3b91581e1
demoMay <105841692+demoMay@users.noreply.github.com>
2022-12-13 21:09:33 +0530

    Shell

---
+++ story_content/6K1A9XuU4AO_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.70.29211.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.717%0D%0AWelcome%20to%20the%20e-learning%20module%20on%20Business%20Law.%0D%0A%0D%0A"}')
