a00848698f759e67db2343f9b35e5b80e1d3307f
demoMay <105841692+demoMay@users.noreply.github.com>
2022-12-19 22:21:27 +0530

    19 Dec
    First Module Updated

---
+++ story_content/5lmdSYrRJlp_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.71.29339.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.011%0D%0AIt%E2%80%99s%20time%20to%20check%20your%20understanding.%C2%A0%0D%0A%0D%0A%0D%0A"}')
